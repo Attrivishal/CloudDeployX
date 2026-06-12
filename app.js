85846daa838e518199e6e6e1289592b1140d107a
Vishal Attri <vishalattri196@gmail.com>
2026-06-12 22:59:41 +0530

    New file added.

--- app.js
+++ app.js
@@ -1,9 +1,13 @@
 const http = require('http');
 
 const server = http.createServer((req, res) => {
-    res.end("CloudDeployX CI/CD is working 🚀");
+    res.end("CloudDeployX CI/CD is working..");
 });
 
-server.listen(3000, ()=> {
-    console.log("Server is running on port 3000");
-});
+if (require.main === module) {
+    server.listen(3000, () => {
+        console.log("Server is running on port 3000");
+    });
+}
+
+module.exports = server;
